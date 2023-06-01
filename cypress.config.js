393ed6a6cd8cd011933ef1f69371707ae9188ac3
Pedro Giron <pedro.giron1864@gmail.com>
2023-06-01 12:01:55 -0600

    Added all the required modules to the project

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
+    },
+    specPattern: "cypress/e2e/*.feature",
+  },
+});
