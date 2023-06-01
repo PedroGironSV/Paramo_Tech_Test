a3835e725f7a24915de285238bcb5640684c0016
Pedro Giron <pedro.giron1864@gmail.com>
2023-06-01 15:19:54 -0600

    Imported all required modules to StepDefinition file

--- cypress/e2e/technical_test/StepDefinition.js
+++ cypress/e2e/technical_test/StepDefinition.js
@@ -1,1 +1,4 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import home from "./page_object_model/Home";
+import registration from "./page_object_model/Registration";
+import login from "./page_object_model/Login";
