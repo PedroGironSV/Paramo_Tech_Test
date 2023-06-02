2cccf2d6e94341c1e1a1f456ccee19e19c9d1428
Pedro Giron <pedro.giron1864@gmail.com>
2023-06-01 21:15:07 -0600

    Implemented steps definition for background

--- cypress/e2e/technical_test/StepDefinition.js
+++ cypress/e2e/technical_test/StepDefinition.js
@@ -2,3 +2,19 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 import home from "./page_object_model/Home";
 import registration from "./page_object_model/Registration";
 import login from "./page_object_model/Login";
+
+home.handleUncaughtExceptions();
+/*
+    STEP DEFINITION FOR BACKGROUND
+*/
+Given("I navigate to demo.casino web site", () => {
+    home.navigateToDemoCasino();
+})
+
+When("I can see the Welcome message", () => {
+    home.waitForWelcomeCard();
+})
+
+Then("I close the Welcome message card", () => {
+    home.closeWelcomeCard();
+})
