0e5513d8d7b49206f74c8343ae469e45b17a14a6
Pedro Giron <pedro.giron1864@gmail.com>
2023-06-01 20:52:36 -0600

    Added modules and actions to interact with the home page

--- cypress/e2e/technical_test/page_object_model/Home.js
+++ cypress/e2e/technical_test/page_object_model/Home.js
@@ -1,5 +1,29 @@
 class HomeActions {
 
+    homeElements = {
+        visitDemoCasino: () => cy.visit('https://demo.casino/', { failOnStatusCode: false }),
+        welcomeCard: () => cy.get('.mfp-content'),
+        closeWelcomeCardButton: () => cy.get('button').contains('Got it').should('be.visible').click(),
+        signUpButton: () => cy.get('a').contains('Sign up')
+    }
+
+    handleUncaughtExceptions() {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false;
+        });
+    }
+
+    navigateToDemoCasino() {
+        this.homeElements.visitDemoCasino();
+    }
+
+    waitForWelcomeCard() {
+        this.homeElements.welcomeCard();
+    }
+
+    closeWelcomeCard() {
+        this.homeElements.closeWelcomeCardButton();
+    }
 }
 
 module.exports = new HomeActions();
