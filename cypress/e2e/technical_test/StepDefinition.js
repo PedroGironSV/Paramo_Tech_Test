8914766631b7a3a4e829eacc747c3382edc0e1d8
Pedro Giron <pedro.giron1864@gmail.com>
2023-06-01 21:30:43 -0600

    Implemented steps definition for User Registration scenario

--- cypress/e2e/technical_test/StepDefinition.js
+++ cypress/e2e/technical_test/StepDefinition.js
@@ -5,7 +5,7 @@ import login from "./page_object_model/Login";
 
 home.handleUncaughtExceptions();
 /*
-    STEP DEFINITION FOR BACKGROUND
+    STEPS DEFINITION FOR BACKGROUND
 */
 Given("I navigate to demo.casino web site", () => {
     home.navigateToDemoCasino();
@@ -17,4 +17,35 @@ When("I can see the Welcome message", () => {
 
 Then("I close the Welcome message card", () => {
     home.closeWelcomeCard();
+})
+
+/*
+    STEPS DEFINITION FOR USER REGISTRATION SCENARIO
+*/
+
+Given("I click on Sing Up button", () => {
+    registration.clickOnSignUpButton();
+})
+
+When("I type my email {string} and password {string}", (email, password) => {
+    registration.typeEmail(email);
+    registration.typePassword(password);
+    registration.confirmPassword(password);
+})
+
+And("I select my rather currency {string}", (currency) => {
+    registration.clickOnCurrencyDropDown();
+    registration.selectRatherCurrency(currency);
+})
+
+Then("I complete the registration form", () => {
+    registration.acceptTermsAndConditions();
+})
+
+And("I select the Create Account button", () => {
+    registration.clickOnCreateAccountButton();
+})
+
+Then("I can see a successful registration message: {string}", (successRegMessage) => {
+    registration.verifySuccessfulRegistrationMessage(successRegMessage);
 })
